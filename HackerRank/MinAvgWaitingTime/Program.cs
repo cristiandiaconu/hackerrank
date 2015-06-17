166e624c33ae50b88d055391cb8ad58b20464893
Cristian Diaconu <cristi.bonecollector@gmail.com>
2015-06-17 10:19:19 +0300

    MinAvgWaitingTime working solution (using priority queues)

--- HackerRank/MinAvgWaitingTime/Program.cs
+++ HackerRank/MinAvgWaitingTime/Program.cs
@@ -13,138 +13,125 @@ namespace MinAvgWaitingTime
     class Program
     {
         private static int N;
-        private static long[] T;
-        private static long[] L;
+
+        public class PriorityQueue<T> where T : IComparable<T>
+        {
+            private List<T> data;
+
+            public PriorityQueue()
+            {
+                this.data = new List<T>();
+            }
+
+            public void Enqueue(T item)
+            {
+                data.Add(item);
+                int ci = data.Count - 1; // child index; start at end
+                while (ci > 0)
+                {
+                    int pi = (ci - 1) / 2; // parent index
+                    if (data[ci].CompareTo(data[pi]) >= 0) break; // child item is larger than (or equal) parent so we're done
+                    T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
+                    ci = pi;
+                }
+            }
+
+            public T Dequeue()
+            {
+                // assumes pq is not empty; up to calling code
+                int li = data.Count - 1; // last index (before removal)
+                T frontItem = data[0];   // fetch the front
+                data[0] = data[li];
+                data.RemoveAt(li);
+
+                --li; // last index (after removal)
+                int pi = 0; // parent index. start at front of pq
+                while (true)
+                {
+                    int ci = pi * 2 + 1; // left child index of parent
+                    if (ci > li) break;  // no children so done
+                    int rc = ci + 1;     // right child
+                    if (rc <= li && data[rc].CompareTo(data[ci]) < 0) // if there is a rc (ci + 1), and it is smaller than left child, use the rc instead
+                        ci = rc;
+                    if (data[pi].CompareTo(data[ci]) <= 0) break; // parent is smaller than (or equal to) smallest child so done
+                    T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp; // swap parent and child
+                    pi = ci;
+                }
+                return frontItem;
+            }
+
+            public T Peek()
+            {
+                T frontItem = data[0];
+                return frontItem;
+            }
+
+            public int Count()
+            {
+                return data.Count;
+            }
+        } // PriorityQueue
+
+        class Customer : IComparable<Customer>
+        {
+            public long T { get; set; }
+            public long L { get; set; }
+
+            public int CompareTo(Customer other)
+            {
+                if (this == other) return 0;
+
+                var cmp = this.L.CompareTo(other.L);
+                if (cmp != 0) return cmp;
+
+                return 1;
+            }
+        }
 
         static void Main(string[] args)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-
             #region Read input
             N = int.Parse(Console.ReadLine());
 
-            var customers = new List<KeyValuePair<long, long>>();
+            var customers = new List<Customer>();
             for (int i = 0; i < N; i++)
             {
                 var values = Console.ReadLine().Split(' ').Select(s => long.Parse(s)).ToList();
-                customers.Add(new KeyValuePair<long, long>(values[0], values[1]));
+                customers.Add(new Customer { T = values[0], L = values[1] });
             }
-
-            Console.WriteLine("Input: " + sw.ElapsedMilliseconds);
-            sw.Reset();
-            sw.Start();
-
-            customers.Sort((x, y) =>
-            {
-                var cmp = x.Key.CompareTo(y.Key);
-                if (cmp == 0)
-                {
-                    return x.Value.CompareTo(y.Value);
-                }
-                return cmp;
-            });
-
-            T = customers.Select(c => c.Key).ToArray();
-            L = customers.Select(c => c.Value).ToArray();
-
-            Console.WriteLine("Sorting: " + sw.ElapsedMilliseconds);
-            sw.Reset();
-            sw.Start();
-
             #endregion
 
-            // SW = total wait time
-            // Li = pizza time of customer i
-            // Ti = time when customer i arrives
-            // ST = sum of all Ti
+            customers.Sort((x, y) => x.T.CompareTo(y.T));
 
-            // the problem is similar to minimizing SL in the following
-            // SW = SL - ST
-            // SL = N * Lk0 + (N-1) * Lk1 + (N-2) * Lk2 + ... + LkN
+            PriorityQueue<Customer> pending = new PriorityQueue<Customer>();
+            pending.Enqueue(customers[0]);
 
-            // so basically find combination k0...kN of 1..N so that SL is minimized.
-
-            var pending = Enumerable.Range(0, N).ToList();
             var time = 0L;
             var SL = 0L;
             var SCL = 0L;
-            var crt = N;
+            var pos = 1;
 
-            Debugger.Launch();
-
-            //List<long> time1 = new List<long>();
-
-            Console.WriteLine("Init: " + sw.ElapsedMilliseconds);
-            sw.Reset();
-            sw.Start();
-
-            while (pending.Any())
+            while (pending.Count() > 0)
             {
-                //Stopwatch sw2 = Stopwatch.StartNew();
+                var customer = pending.Dequeue();
 
-                var min = T[pending[0]];
-                var max = min > time ? min : time;
-
+                var min = customer.T;
                 var deadTime = min > time ? min - time : 0L;
 
-                var fastest = 0;
-                var alreadyCookedFastestPizzaTime = L[pending[fastest]];
-
-                if (deadTime == 0)
-                {
-                    var ctLast = 0;
-                    while (true)
-                    {
-                        ctLast++;
-
-                        if (ctLast >= pending.Count)
-                        {
-                            break;
-                        }
-
-                        var crtPending = pending[ctLast];
-                        if (T[crtPending] > max)
-                        {
-                            break;
-                        }
-
-                        var cookingC = L[crtPending];
-                        if (cookingC < alreadyCookedFastestPizzaTime)
-                        {
-                            fastest = ctLast;
-                            alreadyCookedFastestPizzaTime = cookingC;
-                        }
-                    }
-                }
-
-                var temp = alreadyCookedFastestPizzaTime + deadTime;
+                var temp = customer.L + deadTime;
                 SCL += temp;
                 SL += SCL;
-                crt--;
                 time += temp;
 
-                pending.RemoveAt(fastest);
-                // time1.Add(sw2.ElapsedMilliseconds);
+                while (pos < customers.Count && customers[pos].T < time)
+                {
+                    pending.Enqueue(customers[pos]);
+                    pos++;
+                }
             }
 
-            Console.WriteLine("Loop: " + sw.ElapsedMilliseconds);
-            sw.Reset();
-            sw.Start();
-
-            //Console.WriteLine("Loop Breakdown: " + time1.Sum() + " " + time1.Average() + " " + time1.Min() + " " + time1.Max() + " " + time1.Count);
-
-            var SW = SL - T.Sum();
-
+            var SW = SL - customers.Sum(c => c.T);
             Console.WriteLine(SW / N);
         }
-
-        //private static int FindCustomerAtTime(long time, List<int> pending, out long deadTime)
-        //{
-        //    deadTime = 0;
-
-
-
-        //    return last;
-        //}
     }
 }
